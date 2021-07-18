af4961101c2b36a1a68dd715801041142cae0993
Wadaha <63825388+Jeong-Sanghun@users.noreply.github.com>
2021-07-18 22:49:05 +0900

    띨빵손님

---
+++ WitchPharmacyProject/Assets/Scripts/ClassScript/RoomCounter/Dialog/RandomVisitorSpecialDialogBundle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RandomVisitorSpecialDialogBundle
+{
+    public string rewardSpecialMedicine;
+    public List<RandomVisitorSpecialDialog> dialogList;
+
+    public RandomVisitorSpecialDialogBundle()
+    {
+        rewardSpecialMedicine = "meltfire";
+    }
+}
